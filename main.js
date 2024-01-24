f2b2e0638879870f6783dd5142021a0f5def7d0d
thatboikham <khamjapher1@gamil.com>
2024-01-24 19:45:59 +0400

    feat: add a function that add a mark to the board

---
+++ main.js
@@ -0,0 +1,27 @@
+function gameBoard(){
+    const board = [];
+    const row = 3;
+    const column = 3;
+  
+  
+    for(let i = 0; i < row; i++){
+      board[i] = []
+      for(let j = 0; j < column; j++){
+        board[i][j] = null
+      }
+    }
+    const getboard = () => board;
+
+    const addMarkToBoard = (columIndex, rowIndex, mark) =>{
+        if(columIndex >= 0 && columIndex < column && rowIndex >= 0 && rowIndex < row ){
+          board[rowIndex][columIndex] = mark
+        } else{
+          console.log("invalid row or column")
+        }
+      }
+
+    return{
+      getboard,
+      addMarkToBoard
+    }
+  }
